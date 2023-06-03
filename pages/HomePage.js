3d745e66c2f66d54c8cae92aea64db912f94ccd2
Odmaral Tumenjargal <odmaraltumenjargal@Odmarals-MacBook-Air.local>
2023-06-03 11:46:28 -0700

    implemented save water and sleep functions

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -12,20 +12,173 @@ import { useFonts } from "expo-font";
 import CheckBox from "@react-native-community/checkbox";
 import Water from "../components/Water";
 import Clock from "../components/Clock";
+import axios from "react-native-axios";
+import { useUserProvider } from "../provider/UserProvider";
+import { SavedModal } from "../components/SavedModal";
 
 export const HomePage = (props) => {
   const [dateState, setDateState] = useState(new Date());
-  useEffect(() => {
-    setInterval(() => setDateState(new Date()), 30000);
-  }, []);
+  const [water, setWater] = useState("");
+  const [sleep, setSleep] = useState("");
+  const { userId } = useUserProvider();
+  const [sleepData, setSleepData] = useState([]);
+  const [waterData, setWaterData] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+
   const { navigation } = props;
 
   const [loaded] = useFonts({
     Mulish1: require("../assets/Mulish1.ttf"),
   });
+  const openSavedModal = () => {
+    setModalVisible(true);
+  };
+  const openUpdatedModal = () => {
+    setModalVisible(true);
+  };
+  const getSleep = async () => {
+    await axios
+      .get(`https://plannify-ny7u.onrender.com/${userId._j}/sleeps`)
+      .then((response) => {
+        setSleepData(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const getWater = async () => {
+    await axios
+      .get(`https://plannify-ny7u.onrender.com/${userId._j}/waters`)
+      .then((response) => {
+        setWaterData(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 30000);
+  }, []);
+  useEffect(() => {
+    getSleep();
+  }, []);
+  useEffect(() => {
+    getWater();
+  }, []);
+
+  const handleSleepChange = (value, name) => {
+    setSleep({ ...sleep, [name]: value, ownerId: userId._j });
+  };
+  const handleWaterChange = (value, name) => {
+    setWater({ ...water, [name]: value, ownerId: userId._j });
+  };
+
+  const updateSleep = async (hoursSlept, minutesSlept, _id) => {
+    await axios
+      .put(
+        `https://plannify-ny7u.onrender.com/sleep/${_id}`,
+        { ...sleep, ...sleepData },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9kZHk5NzZAZ21haWwuY29tIiwidXNlcklkIjoiNjQyNGM4ZmU1ZGE0YTU2YjNmZmFkYjlkIiwiaWF0IjoxNjgwMjMyNTg3LCJleHAiOjE2ODAzMTg5ODd9.NknlP8Swrw7dqmh5ABwdNs-WLyGK2XAUjFk7FkCqkJc`,
+          },
+        }
+      )
+      .then((response) => {
+        openUpdatedModal();
+      })
+      .catch((err) => console.log(err));
+  };
+  const updateWater = async (cupsDrank, _id) => {
+    await axios
+      .put(
+        `https://plannify-ny7u.onrender.com/water/${_id}`,
+        { ...water, ...waterData },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9kZHk5NzZAZ21haWwuY29tIiwidXNlcklkIjoiNjQyNGM4ZmU1ZGE0YTU2YjNmZmFkYjlkIiwiaWF0IjoxNjgwMjMyNTg3LCJleHAiOjE2ODAzMTg5ODd9.NknlP8Swrw7dqmh5ABwdNs-WLyGK2XAUjFk7FkCqkJc`,
+          },
+        }
+      )
+      .then((response) => {
+        openUpdatedModal();
+      })
+      .catch((err) => console.log(err));
+  };
+  const createSleepValues = async (sleep) => {
+    await axios.post(`https://plannify-ny7u.onrender.com/sleep`, sleep, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+  const createWaterValues = async (water) => {
+    await axios.post(
+      `https://plannify-ny7u.onrender.com/water`,
+      water,
+      { cupsTotal: "8" },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  };
+  const handleSaveSleepButton = async (e) => {
+    e.preventDefault();
+    const values = {
+      ...sleep,
+    };
+    if (!values.minutesSlept) {
+      values.minutesSlept = "0";
+    }
+    if (sleepData.length <= 0) {
+      if (!values.hoursSlept) {
+        setError(true);
+      }
+
+      await createSleepValues(values)
+        .then(async (response) => {
+          openSavedModal();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    await updateSleep(
+      sleepData[sleepData.length - 1]?.hoursSlept,
+      sleepData[sleepData.length - 1]?.minutesSlept,
+      sleepData[sleepData.length - 1]?._id
+    );
+  };
+  const handleSaveWaterButton = async (e) => {
+    e.preventDefault();
+    const values = {
+      ...water,
+    };
+    if (waterData.length <= 0) {
+      if (!values.cupsDrank) {
+        setError(true);
+      }
+      if (!values.cupsTotal) {
+        values.cupsTotal = "8";
+      }
+
+      await createWaterValues(values)
+        .then(async (response) => {
+          openSavedModal();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    await updateWater(
+      waterData[waterData.length - 1]?.cupsDrank,
+      waterData[waterData.length - 1]?._id
+    );
+  };
   if (!loaded) {
     return null;
   }
+
   return (
     <View style={styles.mainDiv}>
       <ImageBackground
@@ -97,21 +250,78 @@ export const HomePage = (props) => {
         <Text style={styles.water}>
           How many cups of water did you drink today?
         </Text>
-        <TextInput style={styles.waterInput} />
+        {waterData && (
+          <TextInput
+            onChangeText={(newText) => {
+              handleWaterChange(newText, "cupsDrank");
+            }}
+            placeholder={waterData[waterData.length - 1]?.cupsDrank || "0"}
+            style={styles.waterInput}
+          />
+        )}
+        {waterData.length < 1 && (
+          <TextInput
+            onChangeText={(newText) => {
+              handleWaterChange(newText, "cupsDrank");
+            }}
+            style={styles.waterInput}
+          />
+        )}
         <View style={styles.waterSvg}>
           <Water />
         </View>
-        <Text style={styles.waterSaveButton}>Save</Text>
+        <Text onPress={handleSaveWaterButton} style={styles.waterSaveButton}>
+          Save
+        </Text>
         <View style={styles.clockSvg}>
           <Clock />
         </View>
         <Text style={styles.hrSleep}>How many hours did you sleep today?</Text>
-        <TextInput style={styles.hrSleepInput} />
+        {sleepData && (
+          <TextInput
+            onChangeText={(newText) => {
+              handleSleepChange(newText, "hoursSlept");
+            }}
+            placeholder={sleepData[sleepData.length - 1]?.hoursSlept || "0"}
+            style={styles.hrSleepInput}
+          />
+        )}
+        {sleepData.length < 1 && (
+          <TextInput
+            onChangeText={(newText) => {
+              handleSleepChange(newText, "hoursSlept");
+            }}
+            style={styles.hrSleepInput}
+          />
+        )}
         <Text style={styles.minSleep}>
           How many minutes did you sleep today?
         </Text>
-        <TextInput style={styles.minSleepInput} />
-        <Text style={styles.sleepSaveButton}>Save</Text>
+        {sleepData && (
+          <TextInput
+            onChangeText={(newText) => {
+              handleSleepChange(newText, "minutesSlept");
+            }}
+            placeholder={sleepData[sleepData.length - 1]?.minutesSlept || "0"}
+            style={styles.minSleepInput}
+          />
+        )}
+        {sleepData.length < 1 && (
+          <TextInput
+            onChangeText={(newText) => {
+              handleSleepChange(newText, "minutesSlept");
+            }}
+            style={styles.minSleepInput}
+          />
+        )}
+        <SavedModal
+          text="You have successfully saved your input."
+          setModalVisible={setModalVisible}
+          modalVisible={modalVisible}
+        />
+        <Text onPress={handleSaveSleepButton} style={styles.sleepSaveButton}>
+          Save
+        </Text>
       </ImageBackground>
     </View>
   );
@@ -144,12 +354,15 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1.2,
     width: 24,
     marginLeft: 275,
+    textAlign: "center",
     marginTop: -20,
   },
   minSleepInput: {
     borderBottomWidth: 1.2,
     width: 24,
     marginLeft: 305,
+    textAlign: "center",
+
     marginTop: -20,
   },
   clockSvg: {
@@ -160,7 +373,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 600,
     marginLeft: 215,
-    marginTop: 8,
+    marginTop: 12,
     color: "#3B3C49",
   },
   waterSvg: {
@@ -172,6 +385,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1.2,
     width: 30,
     marginTop: -20,
+    textAlign: "center",
     marginLeft: 170,
   },
   water: {
@@ -179,7 +393,7 @@ const styles = StyleSheet.create({
     fontWeight: 600,
     width: 220,
     marginLeft: 40,
-    marginTop: 210,
+    marginTop: 220,
     color: "#3B3C49",
   },
   scrollDiv: { maxHeight: 100 },
